88bc97f9d6a723e012ad916be44705f45af42452
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2021-11-22 02:45:57 +0700

    first upload

---
+++ dist/Form.d.ts
@@ -0,0 +1,28 @@
+import { default as React } from 'react';
+import { Result as ValResult, ValidationProps } from '@nodestrap/validations';
+import { ContentProps } from '@nodestrap/content';
+import { ValidatorHandler } from '@nodestrap/editable-control';
+export declare type CustomValidatorHandler = (isValid: ValResult) => ValResult;
+export declare const useFormValidator: (customValidator?: CustomValidatorHandler | undefined) => {
+    /**
+     * Handles the validation result.
+     * @returns
+     * `null`  = uncheck.
+     * `true`  = valid.
+     * `false` = invalid.
+     */
+    validator: ValidatorHandler;
+    handleInit: (target: HTMLFormElement) => void;
+    handleChange: (target: HTMLFormElement) => void;
+};
+export declare const usesFormLayout: () => import("@cssfn/cssfn").StyleCollection;
+export declare const usesFormVariants: () => import("@cssfn/cssfn").StyleCollection;
+export declare const usesFormStates: () => import("@cssfn/cssfn").StyleCollection;
+export declare const useFormSheet: import("@cssfn/types").Factory<import("jss").Classes<"main">>;
+export declare const cssProps: import("@cssfn/css-config").Refs<{}>, cssDecls: import("@cssfn/css-config").Decls<{}>, cssVals: import("@cssfn/css-config").Vals<{}>, cssConfig: import("@cssfn/css-config").CssConfigSettings;
+export interface FormProps extends ContentProps<HTMLFormElement>, React.FormHTMLAttributes<HTMLFormElement>, ValidationProps {
+    customValidator?: CustomValidatorHandler;
+    children?: React.ReactNode;
+}
+export declare function Form(props: FormProps): JSX.Element;
+export { Form as default };
---
+++ dist/Form.jsx
@@ -0,0 +1,190 @@
+// react:
+import { default as React, useState, } from 'react'; // base technology of our nodestrap components
+// cssfn:
+import { 
+// compositions:
+composition, mainComposition, imports, 
+// layouts:
+layout, 
+// rules:
+states, } from '@cssfn/cssfn'; // cssfn core
+import { 
+// hooks:
+createUseSheet, } from '@cssfn/react-cssfn'; // cssfn for react
+import { createCssConfig, 
+// utilities:
+usesGeneralProps, usesSuffixedProps, overwriteProps, } from '@cssfn/css-config'; // Stores & retrieves configuration using *css custom properties* (css variables)
+// nodestrap utilities:
+import { 
+// utilities:
+setRef, } from '@nodestrap/utilities';
+import { usePropValidation, ValidationProvider, } from '@nodestrap/validations';
+// nodestrap components:
+import { 
+// hooks:
+usesSizeVariant, } from '@nodestrap/basic';
+import { 
+// styles:
+usesContentLayout, usesContentVariants, Content, } from '@nodestrap/content';
+import { 
+// hooks:
+isValid, isInvalid, usesValidInvalidState, markValid, markInvalid, useValidInvalidState, } from '@nodestrap/editable-control';
+export const useFormValidator = (customValidator) => {
+    // states:
+    let [isValid, setIsValid] = useState(null);
+    const handleValidation = (target, immediately = false) => {
+        const getIsValid = () => target.matches(':valid') ? true : (target.matches(':invalid') ? false : null);
+        const update = (validPrev) => {
+            const validNow = getIsValid();
+            if ((validPrev !== undefined) && (validPrev !== validNow))
+                return; // the validity has been modified during waiting => abort further validating
+            // instantly update variable `isValid` without waiting state to refresh (re-render)
+            // because the value is needed immediately by `useValidInvalidState` at startup
+            isValid = (customValidator ? customValidator(validNow) : validNow);
+            setIsValid(isValid);
+        };
+        if (immediately) {
+            // instant validate:
+            update();
+        }
+        else {
+            const validPrev = getIsValid();
+            // delay a while for the further validating, to avoid unpleasant splash effect
+            setTimeout(() => update(validPrev), (validPrev !== false) ? 300 : 600);
+        } // if
+    };
+    const handleInit = (target) => {
+        handleValidation(target, /*immediately =*/ true);
+    };
+    const handleChange = (target) => {
+        handleValidation(target);
+    };
+    return {
+        /**
+         * Handles the validation result.
+         * @returns
+         * `null`  = uncheck.
+         * `true`  = valid.
+         * `false` = invalid.
+         */
+        validator: (() => isValid),
+        handleInit: handleInit,
+        handleChange: handleChange,
+    };
+};
+//#endregion validInvalid
+// styles:
+export const usesFormLayout = () => {
+    return composition([
+        imports([
+            // layouts:
+            usesContentLayout(),
+        ]),
+        layout({
+            // customize:
+            ...usesGeneralProps(cssProps), // apply general cssProps
+        }),
+    ]);
+};
+export const usesFormVariants = () => {
+    // dependencies:
+    // layouts:
+    const [sizes] = usesSizeVariant((sizeName) => composition([
+        layout({
+            // overwrites propName = propName{SizeName}:
+            ...overwriteProps(cssDecls, usesSuffixedProps(cssProps, sizeName)),
+        }),
+    ]));
+    return composition([
+        imports([
+            // variants:
+            usesContentVariants(),
+            // layouts:
+            sizes(),
+        ]),
+    ]);
+};
+export const usesFormStates = () => {
+    // dependencies:
+    // states:
+    const [validInvalid] = usesValidInvalidState();
+    return composition([
+        imports([
+            // states:
+            validInvalid(),
+        ]),
+        states([
+            isValid([
+                imports([
+                    markValid(),
+                ]),
+            ]),
+            isInvalid([
+                imports([
+                    markInvalid(),
+                ]),
+            ]),
+        ]),
+    ]);
+};
+export const useFormSheet = createUseSheet(() => [
+    mainComposition([
+        imports([
+            // layouts:
+            usesFormLayout(),
+            // variants:
+            usesFormVariants(),
+            // states:
+            usesFormStates(),
+        ]),
+    ]),
+]);
+// configs:
+export const [cssProps, cssDecls, cssVals, cssConfig] = createCssConfig(() => {
+    return {
+    /* no config props yet */
+    };
+}, { prefix: 'frm' });
+export function Form(props) {
+    // styles:
+    const sheet = useFormSheet();
+    // states:
+    const formValidator = useFormValidator(props.customValidator);
+    const validInvalidState = useValidInvalidState(props, formValidator.validator);
+    // fn props:
+    const propValidation = usePropValidation(props);
+    // jsx:
+    return (<Content 
+    // other props:
+    {...props} 
+    // semantics:
+    semanticTag={props.semanticTag ?? 'form'} semanticRole={props.semanticRole ?? 'form'} 
+    // variants:
+    mild={props.mild ?? true} 
+    // classes:
+    mainClass={props.mainClass ?? sheet.main} stateClasses={[...(props.stateClasses ?? []),
+            validInvalidState.class,
+        ]} 
+    // validations:
+    elmRef={(elm) => {
+            setRef(props.elmRef, elm);
+            if (elm) {
+                formValidator.handleInit(elm);
+            } // if
+        }} onChange={(e) => {
+            props.onChange?.(e);
+            // validations:
+            formValidator.handleChange(e.currentTarget);
+        }} 
+    // events:
+    onAnimationEnd={(e) => {
+            props.onAnimationEnd?.(e);
+            // validations:
+            validInvalidState.handleAnimationEnd(e);
+        }}>
+            {props.children && <ValidationProvider {...propValidation}>
+                {props.children}
+            </ValidationProvider>}
+        </Content>);
+}
+export { Form as default };
